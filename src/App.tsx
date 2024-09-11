4ece8c1e7bc5c6a6a57dd60c0d3fb72159d92484
nanafromjeju <nanafromjeju@gmail.com>
2024-09-11 15:47:28 +0900

    로컬 변경 사항 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,18 @@
-import '@/App.css'
-import GlobalStyles from '@/styles/GlobalStyle'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from '@/pages/Login'
-import AddPlaylist from '@/pages/AddPlaylist'
-import Bookmark from '@/pages/Bookmark'
-import MyPlaylist from '@/pages/MyPlaylist'
-import Networking from '@/pages/Networking'
-import Profile from '@/pages/Profile'
-import SignUp from '@/pages/SignUp'
-import Header from '@/components/theHeader/Header'
-import Navigation from './components/theNavigation/Navigation'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Header />
-        <Navigation />
+      <Router>
         <Routes>
           <Route
-            path="/login"
-            element={<Login />}
-          />
-          <Route
-            path="/signup"
-            element={<SignUp />}
-          />
-          <Route
-            path="/addplaylist"
-            element={<AddPlaylist />}
-          />
-          <Route
-            path="/bookmark"
-            element={<Bookmark />}
-          />
-          <Route
-            path="/myplaylist"
-            element={<MyPlaylist />}
-          />
-          <Route
-            path="/networking"
-            element={<Networking />}
-          />
-          <Route
-            path="/profile"
-            element={<Profile />}
+            path="/"
+            element={<Home />}
           />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   )
 }
